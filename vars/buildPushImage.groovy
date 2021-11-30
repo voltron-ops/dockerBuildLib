9004567445ff81f1e8e094e711b5c538a0373d75
Mohd Kashif Khan <kashifofme@gmail.com>
2021-11-30 21:58:31 +0530

    Updated buildPushImage

--- vars/buildPushImage.groovy
+++ vars/buildPushImage.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]){
     def dockerImage = docker.build("${config.registry}:${env.BUILD_ID}", '.')
-    docker.withRegistry('https://registry.hub.docker.com', 'config.dockerID'){
+    docker.withRegistry('https://registry.hub.docker.com', '${config.dockerID}'){
         dockerImage.push()
     }
     sh "docker rmi ${config.registry}:${env.BUILD_ID}"
