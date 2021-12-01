cd247c42758fb5f6e61f90d235ae6a1658c05b49
Mohd Kashif Khan <kashifofme@gmail.com>
2021-12-01 17:47:22 +0530

    Updated gitCheckout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,6 +1,8 @@
 def call(Map config = [:]){
-    sh checkout([$class: 'GitSCM',
-                 branches: [[name: config.branch ]],
-                 extensions: [],
-                 userRemoteConfigs: [[url: config.gitrepo ]]])
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: config.branch ]],
+        extensions: [],
+        userRemoteConfigs: [[url: config.gitrepo ]]
+    ])
 }
